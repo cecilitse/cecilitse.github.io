b86794d2f45c40976179419ab01e4db16fcdeabc
Cecile Veneziani <contact@cecilitse.org>
2017-04-07 18:41:54 +0900

    Setup middleman build for Webpack and Middleman 4

--- config.rb
+++ config.rb
@@ -1,6 +1,12 @@
 # HACK: Avoid "Option :lang / :locals is not supported by Slim::Engine" warning messages
 #       https://github.com/middleman/middleman/issues/1077
 ::Slim::Engine.set_default_options lang: I18n.locale, locals: {}
+::Sass.load_paths << File.join(root, 'node_modules')
+
+set :css_dir,     'stylesheets'
+set :fonts_dir,   'fonts'
+set :images_dir,  'images'
+set :js_dir,      'javascripts'
 
 # activate :bower
 activate :autoprefixer
@@ -22,17 +28,17 @@ activate :i18n, langs: [:en, :fr]
 activate :livereload
 activate :protect_emails
 
-set :css_dir,     'stylesheets'
-set :fonts_dir,   'fonts'
-set :images_dir,  'images'
-set :js_dir,      'javascripts'
-
 configure :build do
+  activate :asset_hash
   activate :minify_css
+  activate :minify_html, remove_input_attributes: false
   activate :minify_javascript
 
   compass_config do |config|
     config.sass_options = { debug_info:     false }
     config.sass_options = { line_comments:  false }
   end
+
+  # "Ignore" JS so webpack has full control.
+  ignore { |path| path =~ /\/([^\/]*)\.js$/ && $1 != 'all.bundle' }
 end

ebc49cda42829a195dc5887f9cedaf599e232c18
Cecile Veneziani <contact@cecilitse.org>
2020-07-19 20:58:12 +0200

    Bump middleman from 4.2.0 to 4.3.7
    - Also bump autoprefixer from 2.7.1 to 2.10.1
    - Remove compass, protect-emails and livereload
    - Extract used compass mixins into mixins file
    - Extract protect emails code into a helper method and JS script

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # HACK: Avoid "Option :lang / :locals is not supported by Slim::Engine" warning messages
 #       https://github.com/middleman/middleman/issues/1077
 ::Slim::Engine.set_default_options lang: I18n.locale, locals: {}
-::Sass.load_paths << File.join(root, 'node_modules')
+::SassC.load_paths << File.join(root, 'node_modules')
 
 set :css_dir,     'stylesheets'
 set :fonts_dir,   'fonts'
@@ -23,19 +23,12 @@ activate :external_pipeline,
   latency: 1
 
 activate :i18n, langs: [:en, :fr]
-activate :livereload
-activate :protect_emails
 
 configure :build do
   activate :asset_hash
   activate :minify_css
   activate :minify_html, remove_input_attributes: false
   activate :minify_javascript
-
-  compass_config do |config|
-    config.sass_options = { debug_info:     false }
-    config.sass_options = { line_comments:  false }
-  end
 
   # "Ignore" JS so webpack has full control.
   ignore { |path| path =~ /\/([^\/]*)\.js$/ && $1 != 'all.bundle' }

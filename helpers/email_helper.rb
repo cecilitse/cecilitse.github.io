8b8036029ccfeb09a725b091ea2f8acc656afc2e
Cecile Veneziani <contact@cecilitse.org>
2022-05-16 10:06:46 +0200

    Remove middleman website

--- helpers/email_helper.rb
+++
@@ -1,7 +0,0 @@
-module EmailHelper
-  def protect_email(email)
-    rotated_email = email.tr('A-Za-z', 'N-ZA-Mn-za-m')
-
-    "#email-protection-#{rotated_email}"
-  end
-end
--- helpers/urls_helper.rb
+++
@@ -1,5 +0,0 @@
-module UrlsHelper
-  def host_url(url)
-    URI.join("http://#{data.site.host}", url)
-  end
-end
